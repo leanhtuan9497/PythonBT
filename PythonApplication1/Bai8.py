41b9b6f8e399466accb666c7a41f0e51bb5ac019
Tuan <leanhtua9497.work@gmail.com>
2020-12-19 18:32:01 -0800

    bai9

--- PythonApplication1/Bai8.py
+++ PythonApplication1/Bai8.py
@@ -3,7 +3,7 @@ newfile = open("SinhVien8.txt","w+")
 for i in range(n):
     hoten = input("Nhap ho ten: ").title()
     newfile.write(hoten+"\n")
-newfile.close
+
 ten = input("Nhap ten can tim: ").lower()
 newfile.seek(0)
 read_ds = newfile.read().strip()
@@ -13,6 +13,14 @@ for x in list:
     if ten in x.lower().split(" ")[-1]:
         print(x)
 
+
+#n = int(input())
+#newfile = open("SinhVien8.txt","w+")
+#for i in range(n):
+#    hoten = input("Nhap ho ten: ").title()
+#    newfile.write(hoten+"\n")
+#newfile.close()
+#ten = input("Nhap ten can tim: ").lower()
 #f = open("SinhVien8.txt","r")
 #for x in f:   
 #    if(ten.lower in x.lower().split(" ")[-1]):
--- PythonApplication1/Bai9.py
+++ PythonApplication1/Bai9.py
@@ -1,1 +1,21 @@
+n = int(input())
+newfile = open("SinhVien9.txt","w+")
+for i in range(n):
+    mssv = input("mssv: ").title()
+    hoten = input("Nhap ho ten: ").title()
+    newfile.write(mssv+","+hoten+"\n")
+mssv_del = input("mssv can xoa: ")
+newfile.seek(0)
+read = newfile.read().strip().split("\n")
+for i in range (len(read)):
+    if mssv_del in read[i]:
+        read.pop(i)
+        break
+print(read)
+
+f = open("SinhVien9.txt","w")
+for i in read:
+    f.write(i+'\n')
+f.close()
+        
 
