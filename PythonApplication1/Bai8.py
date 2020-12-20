233b5b360b8a1e2fd710c0a9b4fdc1b51c61a078
Tuan <leanhtua9497.work@gmail.com>
2020-12-20 08:30:08 -0800

    first commit

---
+++ PythonApplication1/Bai8.py
@@ -0,0 +1,19 @@
+n = int(input())
+newfile = open("SinhVien8.txt","w+")
+for i in range(n):
+    hoten = input("Nhap ho ten: ").title()
+    newfile.write(hoten+"\n")
+newfile.close
+ten = input("Nhap ten can tim: ").lower()
+newfile.seek(0)
+read_ds = newfile.read().strip()
+list = read_ds.split("\n")
+
+for x in list:   
+    if ten in x.lower().split(" ")[-1]:
+        print(x)
+
+#f = open("SinhVien8.txt","r")
+#for x in f:   
+#    if(ten.lower in x.lower().split(" ")[-1]):
+#        print(x.split(" ")[-1])
